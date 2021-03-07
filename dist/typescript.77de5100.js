e62f33e0467cc86f279637621b329758073fd03d
Fabian Soosaithasan <fabian.soosaithasan@gmail.com>
2021-03-07 12:50:10 +0100

    Enums in typescript

--- dist/typescript.77de5100.js
+++ dist/typescript.77de5100.js
@@ -123,15 +123,44 @@ var printCryptoDetails = function printCryptoDetails(_a) {
   var name = _a.name,
       abbreviation = _a.abbreviation,
       price = _a.price;
-  console.log(name);
-  return name;
+  console.log({
+    name: name,
+    price: price
+  });
+  return {
+    name: name,
+    price: price
+  };
 };
 
 printCryptoDetails({
   name: "Binance Coin",
   abbreviation: "BNB",
   price: 240
-});
+}); // Enums used for example: if you need a list of exact items
+// numeric enums
+
+var CarTypeNums;
+
+(function (CarTypeNums) {
+  CarTypeNums[CarTypeNums["ELECTRIC"] = 0] = "ELECTRIC";
+  CarTypeNums[CarTypeNums["HYBRID"] = 1] = "HYBRID";
+  CarTypeNums[CarTypeNums["PETROL"] = 2] = "PETROL";
+})(CarTypeNums || (CarTypeNums = {})); // string enums
+
+
+var CarType;
+
+(function (CarType) {
+  CarType["ELECTRIC"] = "ELECTRIC";
+  CarType["HYBRID"] = "HYBRID";
+  CarType["PETROL"] = "PETROL";
+})(CarType || (CarType = {}));
+
+var createCarType = function createCarType(carType) {};
+
+createCarType(CarType.ELECTRIC);
+console.log(CarType.ELECTRIC);
 },{}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- index.ts
+++ index.ts
@@ -2,13 +2,37 @@
 
 interface CryptoCurrency {
   name: string;
-  abbreviation: string;
+  abbreviation?: string;
   price: number;
 }
 
-const printCryptoDetails = ({ name, abbreviation, price }: CryptoCurrency) => {
-  console.log(name);
-  return name;
+const printCryptoDetails = ({
+  name,
+  abbreviation,
+  price,
+}: CryptoCurrency): CryptoCurrency => {
+  console.log({ name, price });
+  return { name, price };
 };
 
 printCryptoDetails({ name: "Binance Coin", abbreviation: "BNB", price: 240 });
+
+// Enums used for example: if you need a list of exact items
+
+// numeric enums
+enum CarTypeNums {
+  ELECTRIC, // 0
+  HYBRID, // 1
+  PETROL, // 2
+}
+
+// string enums
+enum CarType {
+  ELECTRIC = "ELECTRIC",
+  HYBRID = "HYBRID",
+  PETROL = "PETROL",
+}
+
+const createCarType = (carType: CarType) => {};
+createCarType(CarType.ELECTRIC);
+console.log(CarType.ELECTRIC);
